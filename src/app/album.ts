a4aee5e8b5bad1171388dd16191f615890da0228
hkwarui <info.warui@gmail.com>
2019-10-28 10:54:57 +0300

    part 4

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,8 @@
 import { Track } from './track'
 
 export interface Album {
-  name: string,
-  releasedate: string,
-  coverimage: string,
+  name: string;
+  releasedate: string;
+  coverimage: string;
   tracks: Track[]
   }
--- src/app/track.ts
+++ src/app/track.ts
@@ -1,8 +1,8 @@
 export interface Track {
-  trackNumber: number,
-  trackName: string,
-  trackLength: string,
-  trackPrice: number,
+  trackNumber: number;
+  trackName: string;
+  trackLength: string;
+  trackPrice: number;
 }
 
 
